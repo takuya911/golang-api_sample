5bca69bf11d47f0eed94a67a962e1c93db7fda08
takuya911 <takuya.engineer911@gmail.com>
2020-10-10 11:11:24 +0900

    sample grpc service起動まで

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -37,3 +37,35 @@ resource "google_cloud_run_service" "graphql" {
     latest_revision = true
   }
 }
+
+# gRPC Service
+resource "google_cloud_run_service" "grpc" {
+  name     = "grpc"
+  location = var.region_tokyo
+  template {
+    spec {
+      container_concurrency = var.grpc_svc_port
+      timeout_seconds       = 30
+      containers {
+        image = var.grpc_img
+        env {
+          name  = "GPRC_SERVICE_PORT"
+          value = var.grpc_svc_port
+        }
+        resources {
+          limits = {
+            "cpu" : "1000m",
+            "memory" : "128Mi"
+          }
+        }
+        ports {
+          container_port = var.grpc_svc_port
+        }
+      }
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -3,7 +3,9 @@ variable "gql_img" {
   default = "gcr.io/golang-portfolio/golang-api_sample/graphql@sha256:a2cf3d76928d5409ec1a17fc7c8afb83e5fd72fbce368cab4cb6b09376acf480"
 }
 
-
+variable "grpc_img" {
+  default = "gcr.io/golang-portfolio/golang-api_sample/grpc@sha256:c131955494d11a5df15855586a82dc679fe7f8a48312a6605a8c63b0e7a403a8"
+}
 
 # 基本変更しない部分
 variable "project_name" {
@@ -23,7 +25,7 @@ variable "gql_svc_port" {
 }
 
 variable "grpc_svc_name" {
-  default="grpc"
+  default="grpc-repftyfivq-an.a.run.app"
 }
 
 variable "grpc_svc_port" {
