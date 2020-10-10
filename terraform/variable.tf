d64b015d3a69b91adddbcd6f8c27a47aa3569f61
takuya911 <takuya.engineer911@gmail.com>
2020-10-10 10:58:01 +0900

    main file created

---
+++ terraform/variable.tf
@@ -0,0 +1,11 @@
+variable "project_name" {
+  default = "golang-portfolio"
+}
+
+variable "zone_tokyo" {
+  default = "asia-northeast1-a"
+}
+
+variable "region_tokyo" {
+  default = "asia-northeast1"
+}
